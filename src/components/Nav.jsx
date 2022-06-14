032901a43ec73df0602029187ce25c8011af41c5
Jude Okagu <juud0010@gmail.com>
2022-06-14 16:42:41 -0400

    fixing

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-import { Container,NavItem, NavDropdown, MenuItem,Navbar } from 'react-bootstrap';
+import { Container,Navbar } from 'react-bootstrap';
 
 const Nav = () => {
   return (
     <div>
         
-<Navbar>
+<Navbar className='nav'>
   <Container>
-    <Navbar.Brand href="/">Logo</Navbar.Brand>
+    <Navbar.Brand href="/">Home</Navbar.Brand>
     <Navbar.Toggle />
     <Navbar.Collapse className="justify-content-end">
       <Navbar.Text>
           <ul className='links'>
-            <li>   <a href="/food">Recipe</a></li>
+            <li>   <a href="/food">Food</a></li>
             <li>   <a href="/cake">Confectionary</a></li>
             <li>   <a href="/drinks">Drinks</a></li>
           </ul>
